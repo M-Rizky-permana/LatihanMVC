eab6e4ce5e9f8580eef2940c2e1a78e083b9aab2
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2020-01-08 03:07:04 +0700

    Add files via upload

---
+++ PBO2-10118050-LatihanMVC/src/edu/rizkypermana/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rizkypermana.latihanmvc.event;
+
+import edu.rizkypermana.latihanmvc.model.PelangganModel;
+
+/**
+ * Nim   : 10118050
+ * Nama  : Muhamad Rizky Permana
+ * Kelas : IF 2
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan); 
+
+    
+}
---
+++ PBO2-10118050-LatihanMVC/src/edu/rizkypermana/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rizkypermana.latihanmvc.model;
+
+import edu.rizkypermana.latihanmvc.view.PelangganView;
+
+/**
+ * Nim   : 10118050
+ * Nama  : Muhamad Rizky Permana
+ * Kelas : IF 2
+ * 
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PelangganView pelanggan = new PelangganView();
+        pelanggan.setVisible(true);
+    }
+    
+}
